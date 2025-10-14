12d435278755729f59ae8e1eff0390bcead80634
stephenawilcox <stephenawilcox@gmail.com>
2025-10-14 16:14:30 -0700

    Created serializer and serializer tb

---
+++ serializer.sv
@@ -0,0 +1,74 @@
+module serializer #(
+    parameter integer in_bit_width = 512,
+    parameter integer out_bit_width = 32
+)
+(
+    input wire clk,
+    input wire reset,
+    input wire data_ready,
+    output reg read_data,
+    input wire [in_bit_width-1:0] data_in,
+    output reg write_data,
+    output reg [out_bit_width-1:0] data_out
+);
+
+localparam integer num_segments = in_bit_width / out_bit_width;
+localparam integer seg_ctr_bw   = $clog2(num_segments);
+
+// 2 states, BUSY is for when it's serializing the buffer
+localparam IDLE = 1'b0, BUSY = 1'b1;
+
+reg current_state, next_state;
+reg [in_bit_width-1:0] buffer;                      // the parallel data being serialized
+reg [seg_ctr_bw-1:0] seg_counter;                   // the segment in the buffer to output
+reg [seg_ctr_bw-1:0] seg_counter_q;                 // next seg_counter
+wire seg_ctr_all_ones;                              // if segment counter is all ones it is on last segment of current buffer
+
+assign seg_ctr_all_ones = &seg_counter;             // reduction AND on seg_counter
+
+// the output data, each segment is wired to buffer to support higher clock freq
+reg [out_bit_width-1:0] out_buffer [num_segments]; 
+genvar i;
+generate
+    for (i = 1; i <= num_segments; i++) begin
+        assign out_buffer[i-1] = buffer[out_bit_width*i - 1: out_bit_width * (i-1)];
+    end
+endgenerate
+
+//STATE TRANSITION LOGIC
+always @(*) begin
+	case (current_state)
+		IDLE: begin
+			next_state = data_ready ? BUSY : IDLE; 
+		end
+		BUSY: begin
+			next_state = seg_ctr_all_ones ? (data_ready ? BUSY : IDLE) : BUSY;
+		end
+		default:
+			next_state = IDLE;
+	endcase
+end
+
+always @(posedge clk) begin
+    if (reset) begin
+        current_state <= IDLE;
+        buffer <= 0;
+        seg_counter <= 0;
+    end
+    else begin
+        current_state <= next_state;
+        buffer <= read_data ? data_in : buffer;
+        seg_counter <= seg_counter_q;
+    end
+end
+
+//OUTPUT LOGIC
+always @ (*) begin
+    // localparam IDLE = 1'b0, BUSY = 1'b1; --> current_state will be IDLE or BUSY
+    read_data = (!current_state & data_ready) | (seg_ctr_all_ones & data_ready);
+    write_data = current_state;
+    seg_counter_q = current_state ? (seg_counter + current_state) : 0; 
+    data_out = out_buffer[seg_counter];
+end
+
+endmodule
---
+++ serializer_tb.sv
@@ -0,0 +1,76 @@
+`timescale 1ns/1ps
+module serializer_tb();
+    parameter integer in_bit_width = 512;
+    parameter integer out_bit_width = 32;
+
+    reg clk, reset;
+
+    reg data_ready;
+    reg [in_bit_width-1:0] data_in;
+
+    wire read_data, write_data;
+    wire [out_bit_width-1:0] data_out;
+
+    integer num_segments = in_bit_width/out_bit_width;
+
+    reg [out_bit_width-1:0] data [16];
+    reg [out_bit_width-1:0] prev_data_out;
+
+    integer i, j, count, wr_count;
+
+    serializer #(.in_bit_width(in_bit_width), .out_bit_width(out_bit_width)) ser_inst (
+        .clk(clk),
+        .reset(reset),
+        .data_ready(data_ready),
+        .read_data(read_data),
+        .data_in(data_in),
+        .write_data(write_data),
+        .data_out(data_out)
+    );
+
+
+    initial begin
+        $dumpfile("serializer.vcd");
+        $dumpvars(0, serializer_tb);
+
+        count = 0;
+        wr_count = 0;
+        prev_data_out = 0;
+
+        reset = 1;
+        clk = 0;
+        #20;
+        reset = 0;
+        
+        data_ready = 1;
+
+        for (j = 0; j < 256; j++) begin
+            if (read_data) begin
+                //setting data
+                for (i = 0; i < num_segments; i++) begin
+                    data[i] = i + (count * num_segments);
+                end
+                //setting data_in
+                for (i = 1; i <= num_segments; i++) begin
+                    //data_in[out_bit_width*i - 1: out_bit_width * (i-1)] = data[i-1];
+                    data_in[out_bit_width*(i-1) +: out_bit_width] = data[i-1];
+                end
+                count++;
+            end
+            if (write_data) begin
+                wr_count++;
+                if (data_out != prev_data_out+1) begin
+                    $display("Error at wr_count %d, data_out: %d, prev_data_out: %d", wr_count, data_out, prev_data_out);
+                end
+                prev_data_out = data_out;
+            end
+            #10;
+        end
+
+        $finish;
+    end
+
+    always #5 clk = ~clk;  // 100 MHz clock with 10ns period
+    //at multiples of 10, clk is low
+
+endmodule
